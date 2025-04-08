847103e2cf12a8a60921850631eadf7fdf4180b8
Mahmoud Zain <zainmahmoud719@gmail.com>
2025-04-08 19:53:21 +0200

    Update MessagingStompWebsocketApplication.java

--- src/main/java/com/example/websocketdemo/MessagingStompWebsocketApplication.java
+++ src/main/java/com/example/websocketdemo/MessagingStompWebsocketApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessagingStompWebsocketApplication {
+public class StompWebsocketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+		SpringApplication.run(StompWebsocketApplication.class, args);
 	}
 }
